2ab69f6a374cadfb8e11e7adaff0e638c6e9ddf6
jesse0430 <106939129+jesse0430@users.noreply.github.com>
2022-06-20 14:32:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-data=pd.read_csv('/app/永慶_北屯區.csv')
+data=pd.read_csv('永慶_北屯區.csv')
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
